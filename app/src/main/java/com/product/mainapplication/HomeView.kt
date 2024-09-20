061d9d84ae89cd55aff8a775f9962892df85e47e
Michael <mischa.schenk@gmail.com>
2024-09-20 14:18:39 +0200

    Added Icons & refactor

--- app/src/main/java/com/product/mainapplication/HomeView.kt
+++ app/src/main/java/com/product/mainapplication/HomeView.kt
@@ -3,7 +3,6 @@ package com.product.mainapplication
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -16,10 +15,10 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
-import com.product.mainapplication.ui.views.DashboardScreen
-import com.product.mainapplication.ui.views.FavoritesScreen
+import com.product.mainapplication.ui.views.AddScreen
 import com.product.mainapplication.ui.views.HomeScreen
 import com.product.mainapplication.ui.views.SettingsScreen
+import com.product.mainapplication.ui.views.ShopScreen
 import com.product.mainapplication.ui.views.TaskScreen
 
 @Composable
@@ -29,14 +28,12 @@ fun HomeView(
     val selectedImage = remember { mutableIntStateOf(0) }
     val placeholderPrompt = stringResource(R.string.prompt_placeholder)
     val placeholderResult = stringResource(R.string.results_placeholder)
-    var prompt by rememberSaveable { mutableStateOf(placeholderPrompt) }
-    var result by rememberSaveable { mutableStateOf(placeholderResult) }
     val uiState by homeViewModel.uiState.collectAsState()
     val context = LocalContext.current
     val navController = rememberNavController()
 
     val bottomNavItems = listOf(
-        "Home", "Dashboard", "Favorites", "Tasks", "Settings"
+        "Home", "Shop", "Add", "Tasks", "Settings"
     )
 
     Scaffold(
@@ -48,11 +45,11 @@ fun HomeView(
                 bottomNavItems.forEachIndexed { _, item ->
                     val iconResourceId = when (item) {
                         "Home" -> R.drawable.ic_home_24
-                        "Recipes" -> R.drawable.ic_dashboard_24
-                        "Favorites" -> R.drawable.ic_code_24
+                        "Shop" -> R.drawable.ic_cart_24
+                        "Add" -> R.drawable.ic_add_circle_24
                         "Tasks" -> R.drawable.ic_schedule_24
                         "Settings" -> R.drawable.ic_account_24
-                        else -> R.drawable.ic_person_24
+                        else -> R.drawable.ic_code_24
                     }
                     val isSelected = currentRoute(navController) == item
                     NavigationBarItem(
@@ -90,8 +87,8 @@ fun HomeView(
                     context
                 )
             }
-            composable("Dashboard") { DashboardScreen() }
-            composable("Favorites") { FavoritesScreen() }
+            composable("Shop") { ShopScreen() }
+            composable("Add") { AddScreen() }
             composable("Tasks") { TaskScreen() }
             composable("Settings") { SettingsScreen() }
         }
--- app/src/main/java/com/product/mainapplication/ui/views/FavoritesScreen.kt
+++ app/src/main/java/com/product/mainapplication/ui/views/FavoritesScreen.kt
@@ -4,6 +4,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
-fun FavoritesScreen() {
-    Text("Favorites Content")
+fun AddScreen() {
+    Text("Add Content")
 }
