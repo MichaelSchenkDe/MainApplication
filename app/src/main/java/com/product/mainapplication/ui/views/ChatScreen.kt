cf517aa38f74f3159dbced34707841df90785d5c
Michael <mischa.schenk@gmail.com>
2024-09-29 17:20:43 +0200

    Chat removal

--- app/src/main/java/com/product/mainapplication/ui/views/ChatScreen.kt
+++ app/src/main/java/com/product/mainapplication/ui/views/ChatScreen.kt
@@ -6,51 +6,24 @@ import android.graphics.ImageDecoder
 import android.net.Uri
 import android.os.Build
 import android.provider.MediaStore
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.product.mainapplication.HomeViewModel
-import com.product.mainapplication.R
-import com.product.mainapplication.UiState
 
 @Composable
 fun ChatScreen() {
-    val selectedImage by remember { mutableStateOf<Bitmap?>(null) }
-
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth() // Ensure the Row fills the available width
-                .background(Color.LightGray) // Set the background color to LightGray
                 .padding(16.dp), // Apply padding after setting the background
         ) {
             Text(
-                text = stringResource(R.string.baking_title),
-                style = MaterialTheme.typography.titleLarge,
+                text = "No Idea",
             )
         }
     }
